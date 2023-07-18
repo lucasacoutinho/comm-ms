8c07524af0883194670403e3909db4c6493af7cc
Lucas Coutinho <luca.coutinho1@gmail.com>
2023-07-18 17:07:31 -0300

    build: improve orchestration and devcontainer

--- auth-api/src/app.ts
+++ auth-api/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import morgan from "morgan";
+import cors from "cors";
 import router from "./routes";
 
 const app: Express = express();
@@ -7,6 +8,7 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
 app.use(express.static("public"));
 
 app.use("/api", router);
